2284470bea39e1a6777e65dbd7922c8eb0c2ddd8
Chris McCormick <chris@mccormick.cx>
2022-01-21 17:08:03 +0800

    Rendering floor greebles.

--- src/smash/core.cljs
+++ src/smash/core.cljs
@@ -1,9 +1,11 @@
 (ns smash.core
   (:require [reagent.core :as r]
             [reagent.dom :as rdom]
-            [applied-science.js-interop :as j]
+            ;[applied-science.js-interop :as j]
             [sitefox.ui :refer [log]]
             ["rot-js" :as ROT]))
+
+(log "loaded")
 
 (defn initial-state []
   {:screen :menu})
@@ -46,15 +48,27 @@
     27 (swap! state assoc :screen :menu)
     nil))
 
-(defn component-menu [state]
-  [:div#menu
-   [:h1 "ball smash" [:br] "dungeon"]
-   [:p "a video game about smashing balls"]
-   (when (:map @state)
-     [:p [:button {:on-click #(swap! state assoc :screen :game)} "resume game"]])
-   [:p [:button {:on-click #(start-game state)} "new game"]]])
+(defn generate-greebles [{:keys [x y width height]}]
+  (let [greeble-count (inc (int (* (ROT/RNG.getUniform) 5)))]
+    (for [_g (range greeble-count)]
+      (let [gs (* (ROT/RNG.getUniform) 5)
+            [rx ry] (map (fn [_] (ROT/RNG.getItem #js [1 2])) (range 2))
+            [gx gy] (map #(* (ROT/RNG.getUniform) (nth [width height] %)) (range 2))
+            [gxf gyf] (map #(+ (* (- (ROT/RNG.getUniform) 0.5) 20) %) [gx gy])]
+        (case (ROT/RNG.getItem #js [:circle :arc])
+          :circle [:circle {:cx (+ x gx) :cy (+ y gy) :r gs
+                            :fill "none"
+                            :stroke "#5A5A56"
+                            :stroke-width 1}]
+          :arc [:path {:d (str "M " (+ x gx) "," (+ y gy)
+                               "A " rx " " ry " "
+                               "0 0 1"
+                               (+ x gxf) " " (+ y gyf))
+                       :fill "none"
+                       :stroke "#5A5A56"
+                       :stroke-width 1}])))))
 
-(defn is-adjacent [[x y] tiles]
+(defn is-adjacent-tile [[x y] tiles]
   (>
    (count
      (filter identity
@@ -63,13 +77,40 @@
                (= (get tiles [(+ ox x) (+ oy y)]) 0))))
    0))
 
+(defn component-menu [state]
+  [:div#menu
+   [:h1 "ball smash" [:br] "dungeon"]
+   [:p "a video game about smashing balls"]
+   (when (:map @state)
+     [:p [:button {:on-click #(swap! state assoc :screen :game)} "resume game"]])
+   [:p [:button {:on-click #(start-game state)} "new game"]]])
+
+(defn component-defs [scale]
+  [:defs
+   [:pattern {:id "hatch"
+              :x 0
+              :y 0
+              :width scale
+              :height scale
+              :pattern-units "userSpaceOnUse"}
+    [:rect {:x 0 :y 0 :width scale :height scale :fill "#FAF8F1"}]
+    [:path {:d (str
+                 "M " 0 ",0"
+                 "V " scale
+                 "M " (/ scale 3) ",0"
+                 "V " scale
+                 "M " (* (/ scale 3) 2) ",0"
+                 "V " scale)
+            :stroke-width 1
+            :stroke "#555"}]]])
+
 (defn component-game [state]
   (js/console.log "state" (clj->js @state))
-  (let [scale 40
-        stroke-width 10
+  (let [scale 30
+        stroke-width 7
         game-map (:map @state)
         {:keys [size rooms corridors tiles]} game-map
-        tile-positions (remove nil?
+        _tile-positions (remove nil?
                                (for [[[x y] v] tiles]
                                  (when (= v 0)
                                    {:key (str "tile" [x y])
@@ -79,7 +120,7 @@
                                     :height scale})))
         adjacent-positions (remove nil?
                                    (for [[[x y] v] tiles]
-                                     (when (and (= v 1) (is-adjacent [x y] tiles))
+                                     (when (and (= v 1) (is-adjacent-tile [x y] tiles))
                                        {:key (str "adjacent" [x y])
                                         :cx (* x scale)
                                         :cy (* y scale)
@@ -94,14 +135,13 @@
                                ys (expand-pos (sort [_y1 _y2]))
                                w (- (second xs) (first xs))
                                h (- (second ys) (first ys))
-                               doors (for [[xy v] _doors]
+                               doors (for [[xy _v] _doors]
                                        (let [[x y] (-> xy name (.split ",") (.map #(js/parseInt %)))]
                                          {:key (str "door" [x y])
                                           :x (* (- x 0.5) scale)
                                           :y (* (- y 0.5) scale)
                                           :width scale
                                           :height scale}))]
-                           (log "doors" doors)
                            {:key (str "room" xs ys)
                             :x (* (first xs) scale)
                             :y (* (first ys) scale)
@@ -126,30 +166,9 @@
            :ref #(when % (.focus %))
            :viewBox (str "-" scale " -" scale " " sx " " sy)
            :width sx
-           :height sy
-           ;:style {:transform "rotate(-25deg)"}
-           ;:style {:transform "scaleY(0.66)"}
-           }
+           :height sy}
 
-     [:defs
-      [:pattern {:id "hatch"
-                 :x 0
-                 :y 0
-                 :width scale
-                 :height scale
-                 :pattern-units "userSpaceOnUse"
-                 ;:pattern-content-units "objectBoundingBox"
-                 }
-       [:rect {:x 0 :y 0 :width scale :height scale :fill "#FAF8F1"}]
-       [:path {:d (str
-                    "M " 0 ",0"
-                    "V " scale
-                    "M " (/ scale 3) ",0"
-                    "V " scale
-                    "M " (* (/ scale 3) 2) ",0"
-                    "V " scale)
-               :stroke-width 1
-               :stroke "#555"}]]]
+     [component-defs scale]
 
      ; draw hatching
 
@@ -182,21 +201,23 @@
 
      ; draw interiors
 
-     (for [{:keys [doors]} room-positions]
-       (for [pos-map doors]
-         [:rect (merge pos-map {:fill "#E9E7DC"})]))
+     (for [{:keys [doors]} room-positions
+           pos-map doors]
+
+         [:rect (merge pos-map {:fill "#E9E7DC"})])
 
      (for [pos-map room-positions]
-
-       [:rect (merge pos-map
-                     {:fill "#E9E7DC"})])
+       [:rect (merge pos-map {:fill "#E9E7DC"})])
 
      (for [pos-map corridor-positions]
        [:rect (merge pos-map {:fill "#E9E7DC"})])
 
      ; draw greebles
+     (for [pos room-positions]
+       (generate-greebles pos))
 
-     ]))
+    (for [pos corridor-positions]
+       (generate-greebles pos))]))
 
 (defn component-main [state]
   [:main
